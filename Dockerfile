86cf66f914748928e4a7f36d747348ce981581c9
Emmanuel Ouzan <Emmanuel.ouzan@gmail.com>
2023-08-23 16:57:29 +0300

    Working Versiongit status! +Slim build +HealthCheckDjango

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,11 @@
-FROM python:3.7.17
-
+FROM python:3.7.17-slim
+# check for curl
 WORKDIR /app
-
 COPY requirements.txt requirements.txt
-
+RUN apt update && apt install -y rsync libpq-dev gcc bzip2 curl
 RUN pip install -r requirements.txt
 
-RUN apt update && apt install -y rsync
-
-COPY gutendex/.env gutendex/.env
+# COPY gutendex/.env gutendex/.env
 
 COPY . .
 
--- entry-point.sh
+++ entry-point.sh
@@ -12,15 +12,13 @@ echo "#### Running collectstatic! ####"
 python ./manage.py collectstatic --noinput
 
 # Transfer static data
-mv /var/www/gutendex/static-root /share-static-files/nginx -v
+mv /var/www/gutendex/static-root /share-static-files/nginx
 
 # Confirm that data has been transferred
 if [ -e "/share-static-files/nginx" ]; then
     # Delete original data
-    rm -r /var/www/gutendex/static-root*
+    rm -r /var/www/gutendex/static-root/*
 fi
-
-
 
 echo "#### Running Server! ####"
 python ./manage.py runserver --insecure 0.0.0.0:8000 
